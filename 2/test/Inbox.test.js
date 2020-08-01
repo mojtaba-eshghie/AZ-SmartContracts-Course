7ce43bebc74da8942609e36a819e12eeb5d80e87
Mojtaba Eshghie <mojtaba.eshghi@ut.ac.ir>
2020-08-01 12:57:55 -0700

    adding the new parts for  and  testing on local ganache network

--- 2/test/Inbox.test.js
+++ 2/test/Inbox.test.js
@@ -12,8 +12,9 @@ const web3 = new Web3(ganache.provider())
 
 
 // Let's define a global accounts variable
-let accounts
-let inbox
+let accounts;
+let inbox;
+const INITIAL_STRING = 'This is the initial string... Hello World!'
 
 
 // Use one of those accounts to deploy the contract
@@ -40,7 +41,7 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(abi)
         .deploy({
             data: bytecode.object,
-            arguments: ["Hi  There!"]
+            arguments: [INITIAL_STRING]
         })
         .send({ 
             from: accounts[0],
@@ -52,6 +53,32 @@ describe('Inbox Smart Contract Tests', () => {
     
     it('Checks to see if the accounts are fetched successfully from ganache (using web3.eth.getAccounts promise)', () => {
         console.log(inbox)
+    })
+
+    it('Does the message variable has a default value?', async () => {
+        // calling a method is going to return a promise, thus we will define the 
+        // arrow function as async to wait for this message property to return to us
+        const message = await inbox.methods.message().call(); // call: cannot modify a contract's data. No transaction receipt.
+        assert.equal(message, INITIAL_STRING);
+        
+    })
+
+    it("Can we set the value of the message correctly?", async () => {
+        let new_message = 'I have something to say.';
+        await inbox.methods.setMessage(new_message).send({
+            from: accounts[0],
+
+        }); // send: can modify the contract's data :: returns a transaction receipt!
+        /**
+         * There is no need to check the tranaction hash (receipt) to see if
+         * it has been fine. If anything with the transaction itself is wrong, the
+         * line will not proceed. But, we need to check the results of transaction
+         * modifying the contract's data.
+         */
+
+        const message = await inbox.methods.message().call();
+        assert.equal(message, new_message);
+
     })
 
 })
